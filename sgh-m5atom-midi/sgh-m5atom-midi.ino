e1d4d4bb44f8390404d269bd31a9549a0aad37b7
TAKURO FUKAMIZU <takuro.f.1987@gmail.com>
2021-01-19 13:36:36 +0900

    とりあえず光らせる

--- sgh-m5atom-midi/sgh-m5atom-midi.ino
+++ sgh-m5atom-midi/sgh-m5atom-midi.ino
@@ -11,11 +11,20 @@
 #define PIN_VOL_SW 21
 #define PIN_VOL 25
 
+#define NUM_RGB_LEDS 30
+
+#define MAX_VELOCITY 100
+#define MIN_VELOCITY 40
+
+CRGB leds[NUM_RGB_LEDS];
+
 // MIDI IN 用
 HardwareSerial midiSerial(1);
 
 // MIDIクラスのインスタンスとして"MIDI"を生成する。
 MIDI_CREATE_INSTANCE(HardwareSerial, midiSerial, MIDI);
+
+bool ledOn = false;
 
 void handleNoteOn(byte channel, byte pitch, byte velocity)
 {
@@ -23,14 +32,43 @@ void handleNoteOn(byte channel, byte pitch, byte velocity)
 
     // できるだけコールバックの中は短くするように、
     // でないとloop関数が遅くなり、リアルタイム性能に悪影響が出てしまう
-    Serial.println("NoteOn");
+    Serial.print("NoteOn: ch=");
+    Serial.print(channel);
+    Serial.print(", pitch=");
+    Serial.print(pitch);
+    Serial.print(", velocity=");
+    Serial.println(velocity);
+
+    if ( MAX_VELOCITY < velocity ) {
+        velocity = MAX_VELOCITY;
+    }
+    if ( velocity < MIN_VELOCITY ) {
+        velocity = MIN_VELOCITY;
+    }
+    int t = (1.0 - (float)((float)(velocity - MIN_VELOCITY) / (float)(MAX_VELOCITY - MIN_VELOCITY))) * 50;
+    Serial.println(t);
+    for(int i = 0; i<NUM_RGB_LEDS; i++) {
+        leds[i] = CRGB::Red;
+        FastLED.show();
+        delay(t);
+    }
 }
 
 void handleNoteOff(byte channel, byte pitch, byte velocity)
 {
     // ノートの鳴り終わりにさせたいことを何でも
     // ベロシティ0のノートオンメッセージもNoteOffとして解釈される(つまりここで処理される)点に注意
-    Serial.println("NoteOff");
+    Serial.print("NoteOff: ch=");
+    Serial.print(channel);
+    Serial.print(", pitch=");
+    Serial.print(pitch);
+    Serial.print(", velocity=");
+    Serial.println(velocity);
+    
+    for(int i = 0; i < NUM_RGB_LEDS; i++) {
+        leds[i] = CRGB::Black;
+    }
+    FastLED.show();
 }
 
 void setup() {
@@ -40,8 +78,13 @@ void setup() {
     MIDI.setHandleNoteOn(handleNoteOn);
     MIDI.setHandleNoteOff(handleNoteOff);
     MIDI.begin(MIDI_CHANNEL_OMNI);
-//    MIDI.turnThruOn();
-  
+
+    delay(50);
+
+    // RGB LED
+    FastLED.addLeds<WS2812, PIN_RGB_LED, GRB>(leds, NUM_RGB_LEDS);
+
+    // Other devices
     pinMode(PIN_STATUS_LED, OUTPUT);
     pinMode(PIN_TOUCH, INPUT);
     pinMode(PIN_VOL_SW, INPUT);
